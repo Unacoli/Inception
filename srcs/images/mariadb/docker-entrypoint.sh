ca477bdd4a2d770059e310624f3d5628a8b425df
Nao Argouse <nargouse@student.42.fr>
2022-12-10 06:38:50 +0100

    problem with debian buster

---
+++ srcs/images/mariadb/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+mysql_install_db
+
+/etc/init.d/mysql start
+
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE"]
+then
+    echo "Database exist."
+else
+
+mysql_secure_installation << _EOF_
+
+Y
+password
+password
+Y
+n
+Y
+Y
+_EOF_
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+
+mysql -u root -p $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/conf.sql
+
+fi
+
+/etc/init.d/mysql stop
+
+exec "$@"
