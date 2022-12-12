05f42b37cc6042781a2e16699691487a896b5c5b
Nao Argouse <nargouse@student.42.fr>
2022-12-12 03:38:34 +0100

    fix mariadb and connecting to wp, add user root and normal user in wp, all selfmade images seems to work as intended, start working on checking everything

--- srcs/images/mariadb/docker-entrypoint.sh
+++ srcs/images/mariadb/docker-entrypoint.sh
@@ -1,33 +1,21 @@
 #!/bin/sh
 
-mariadb_install_db
-
-mariadbd
-
-if [ -d "/var/lib/mysql/$MYSQL_DATABASE"]
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
 then
-
-    echo "Database exist."
+    echo "Database exists."
 else
+    mysql_install_db
 
-mysql_secure_installation << _EOF_
-
-Y
-password
-password
-Y
-n
-Y
-Y
-_EOF_
-
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
-
-mysql -u root -p $MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/conf.sql
-
+    /etc/init.d/mysql start
+    echo avant route
+    echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+    echo apres route
+    echo avant nargouz
+    echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u root
+    echo apres nargouz
+    /etc/init.d/mysql stop
+    echo staupe
 fi
 
-/etc/init.d/mysql stop
-
+echo exek
 exec "$@"
--- srcs/images/wordpress/docker-entrypoint.sh
+++ srcs/images/wordpress/docker-entrypoint.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-#check if wp-config.php exist
+# Check if wp-config.php exists
 if [ -f ./wp-config.php ]
 then
 	echo "Wordpress downloaded"
 else
-
-	#Download wordpress and all config file
+	# Download wordpress and all config file
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	#Inport env variables in the config file
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
+	# Import env variables in the config file
 	cp wp-config-sample.php wp-config.php
-
+	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+	
+	# Create users in WP
+	wp --allow-root user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_MAIL --user_pass=$WORDPRESS_ADMIN_PASS --role=administrator
+	wp --allow-root user create $WORDPRESS_USER_USER $WORDPRESS_USER_MAIL --user_pass=$WORDPRESS_USER_PASS
 fi
 
 exec "$@"
